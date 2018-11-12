fd91aded904571dc95ee86abc170453b814717f6
gcarrascoso <43757553+gcarrascoso@users.noreply.github.com>
2018-11-12 13:09:24 -0500

    Update problem-set-4.js

--- pset4/js/problem-set-4.js
+++ pset4/js/problem-set-4.js
@@ -4,7 +4,8 @@
 
 function hello() {
 
-  // WRITE YOUR EXERCISE 1 CODE HERE
+  var p=document.getElementById("output1");
+  p.innerHTML="Hello, AP Computer Science Principles!";
 
   ////////////////// DO NOT MODIFY
   check("hello"); // DO NOT MODIFY
@@ -24,7 +25,9 @@ function helloAgain() {
   // Use the name variable declared above to store the user's response. You
   // do not need to re-declare it, only assign it a value.
 
-  // WRITE YOUR EXERCISE 2 CODE HERE
+  name = prompt('Please enter your name.');
+  var p=document.getElementById("output2");
+  p.innerHTML="Hello, " + name + "!";
 
   ///////////////////////////// DO NOT MODIFY
   check("helloAgain", name); // DO NOT MODIFY
@@ -44,7 +47,9 @@ function celsius() {
   // The above code generates a random number between -100 and 1000
   // (inclusive), and rounds this value to 2 decimal places.
 
-  // WRITE YOUR EXERCISE 3 CODE HERE
+  var product = ((cels * 9/5) + 32).toFixed(2);
+  var p=document.getElementById("output3");
+  p.innerHTML=cels + " degrees Celsius equals " + product + " degrees Fahrenheit.";
 
   ////////////////////////// DO NOT MODIFY
   check("celsius", cels); // DO NOT MODIFY
